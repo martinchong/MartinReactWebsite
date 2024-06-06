688b42107fa9113338b1c30eb56ef7ad9cefcd92
Martin Chong <martinmanho@gmail.com>
2024-06-06 18:00:17 -0400

    Misc bug fix
    Typo fix

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -19,7 +19,7 @@ const Intro = () => {
       <div id="about-me" className="intro-container--about-me">
         <div className='intro-container'>
           <Typography variant='h1' color='primary'>About <span className='intro-container__name-highlight'>Me</span></Typography>
-          <Typography color='primary' className='intro-container__my-story'>So who am I? Well, that is a question I ask myself everyday. To be honest, I think I am still searching for the answer, but right now I am a Fullstack Developer with years of expereince working in a start up company. A developer who is loyal and truthful. If you wanna know me better, do not hesitate to shoot me a <a href="mailto:martinmanho@gmail.com">message</a>! You can also just check out my <a href={pdfUrl} target="_blank" rel="noreferrerYY">resume</a> to see if there is anything inside that can pique your interest.</Typography>
+          <Typography color='primary' className='intro-container__my-story'>So who am I? Well, that is a question I ask myself everyday. To be honest, I think I am still searching for the answer, but right now I am a Fullstack Developer with years of expereince working in a start up company. A developer who is loyal and truthful. If you wanna know me better, do not hesitate to shoot me a <a href="mailto:martinmanho@gmail.com">message</a>! You can also just check out my <a href={pdfUrl} target="_blank" rel="noreferrer">resume</a> to see if there is anything inside that can pique your interest.</Typography>
         </div>
         <Box
           className='intro-container__my-image'
